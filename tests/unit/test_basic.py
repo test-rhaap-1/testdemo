b62ded38286e1aa543c033d240882e51c94f4bf1
Scaffolder <scaffolder@backstage.io>
2024-07-23 13:45:51 +0000

    initial commit

---
+++ tests/unit/test_basic.py
@@ -0,0 +1,6 @@
+"""Unit tests for iamareallylongcollectionnamespacewhichwewanttotestthecollectionnamespacemayormaynothaveasizecheck.correctname."""
+
+
+def test_basic() -> None:
+    """Dummy unit test that always passes."""
+    assert True
